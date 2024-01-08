68a59bcddd4b3c0ac37547e8e827550026099c19
Pedro <ph4290245@gmail.com>
2024-01-08 17:27:08 -0300

    User entity and resource

--- src/main/java/com/teste/demo/DemoApplication.java
+++ src/main/java/com/teste/demo/DemoApplication.java
@@ -1,19 +1,14 @@
 package com.teste.demo;
 
-import com.teste.demo.domain.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("hello");
+
 	}
 
 }
---
+++ src/main/java/com/teste/demo/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.teste.demo.resources;
+
+import com.teste.demo.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L,"maria","maria@gmail.com","999999","12345");
+        return ResponseEntity.ok().body(u);
+
+    }
+
+
+}
